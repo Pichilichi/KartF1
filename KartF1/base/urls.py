f6f93d77ae16d4a129b69daa4c0e201894545ffd
Francisco García Macías <garmacfco@gmail.com>
2023-06-01 19:45:42 +0200

    HTML II
    Booking, pagina de crud sin funciones, retoques pagina principal

--- KartF1/base/urls.py
+++ KartF1/base/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('room/', views.room, name="room"),
+    path('booking/<str:pk>/', views.booking, name="booking"),
+
+    path('create-booking/', views.createBooking, name="create-booking"),
 ]
--- KartF1/base/views.py
+++ KartF1/base/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Booking
 
 def home(request):
-    return render(request, 'home.html')
+    bookings = Booking.objects.all()
+    context = {'bookings': bookings}
+    return render(request, 'home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def booking(request, pk):
+    booking = Booking.objects.get(id=pk)
+    context = {'booking': booking}
+    return render(request, 'booking.html', context)
+
+def createBooking(request):
+    context = {}
+    return render(request, 'booking_form.html',context)
