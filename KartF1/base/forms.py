abf6d9e8b56608052c0e7ca036548b73552c4792
Francisco García Macías <garmacfco@gmail.com>
2023-06-08 23:30:02 +0200

    HTML XVI
    Retoques generales.

--- KartF1/base/forms.py
+++ KartF1/base/forms.py
@@ -1,8 +1,12 @@
-from django.forms import ModelForm
+from django.forms import ModelForm,DateInput
 from .models import Booking
+
 
 class BookingForm(ModelForm):
     class Meta:
         model = Booking
         fields = '__all__'
         exclude = ['user', 'racers']
+        widgets = {
+            'raceDay' : DateInput(attrs={'type':'date'})
+        }
